cf90f514843a0adbef07ec695337222f0034e53b
tundeaji1232 <ajiboye.tunde@gmail.com>
2018-02-05 11:45:19 +0000

    more setup file added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,1 +1,19 @@
-import React from "react";
+import React, { Component} from "react";
+import { connect } from "react-redux";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import * as actions from "../actions";
+
+
+class App extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <Route path="/" component={LandingPage} />
+      </BrowserRouter>
+    )
+  }
+}
+
+
+export default connect(null,actions)(App);
+//in the props of app we can have access to the actions or the state
--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import thunk from "redux-thunk"
+import App from "./components/App"
+import "./styles"
+import reducers from "./reducers"
+
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDOM.render(
+<Provider store={store} >
+  <App />
+</Provider>,
+document.querySelector('#root')
+
+);
