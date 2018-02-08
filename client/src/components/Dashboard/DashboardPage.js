69ace701da1936f2668db7eedc0a79409ec6114b
dragomir <svedava@mail.ru>
2018-02-08 11:23:59 +0000

    displayed tags on the dashboard in alphabetical order fixes #26 and added filtering feature to the dashborad searbar fixes #27

--- client/src/components/Dashboard/DashboardPage.js
+++ client/src/components/Dashboard/DashboardPage.js
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { sortBy, mapValues } from "lodash";
+import { sortBy, filter } from "lodash";
 import * as actions from "../../actions";
 import SearchBar from "../LandingPage/SearchBar";
 import FilterOptions from "./FilterOptions";
-import { Title } from "../../styles";
+
+import CardDisplay from "../LandingPage/CardDisplay";
 
 class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
+  }
+
+  updateSearch = e => {
+    this.setState({ search: e.target.value });
+  };
   render() {
-    const tags = mapValues(this.props.tags, o => o.name);
-    const tagNames = Object.values(tags);
-    console.log(tagNames.sort());
+    console.log(this.props.assets);
+    let filteredAssets = filter(
+      this.props.assets,
+      asset =>
+        asset.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+    );
+
     return (
       <div>
-        <SearchBar />
+        <input
+          type="text"
+          value={this.state.search}
+          onChange={this.updateSearch}
+        />
         <ul className="tags-container">
           <FilterOptions tags={this.props.tags} />
+          {/* <CardDisplay assets={filteredAssets} end={"10"} /> */}
         </ul>
       </div>
     );
--- client/src/components/Dashboard/FilterOptions.js
+++ client/src/components/Dashboard/FilterOptions.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { mapValues } from "lodash";
+import { v4 } from "uuid";
 
-export default ({ tags }) =>
-  Object.values(tags)
-    .slice(0, 6)
-    .map(tag => (
-      <li key={tag.id}>
-        <h1>{tag.name}</h1>
-      </li>
-    ));
+export default ({ tags }) => {
+  const tagNames = Object.values(mapValues(tags, o => o.name)).sort();
+
+  return tagNames.slice(0, 6).map(tag => (
+    <li key={v4()}>
+      <h1>{tag}</h1>
+    </li>
+  ));
+};
