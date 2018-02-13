96028989b56c1f97f2e4eb33cdc6aa332dc8c27c
tundeaji1232 <ajiboye.tunde@gmail.com>
2018-02-13 17:49:39 +0000

    more fix to searchbar

---
+++ client/src/components/Dashboard/SearchByTerm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export default () => {
+   const onChange = e => {
+     onSearchTerm(e.currentTarget.value);
+   };
+   return (
+     <div>
+      <input
+        type = "text"
+        value= {"searchTerm"}
+        onChange={onChange}
+        placeholder="enter search term..."
+      />
+    
+
+
+     </div>
+   )
+}
