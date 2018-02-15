9ba6809403ebd81d70fe7dcf51b4e6e153f685c7
trc5019 <trc5019@gmail.com>
2018-02-15 17:44:12 +0000

    implemented suggested client fixes

--- client/src/components/LandingPage/CardDisplay.js
+++ client/src/components/LandingPage/CardDisplay.js
@@ -5,33 +5,34 @@ import arrow from "../../images/arrow-button.svg";
 import _ from "lodash";
 import { v4 } from "uuid";
 
-export default ({ assets, end, subtags }) => Object.values(assets)
-  .slice(0, end)
-  .map(asset => (
-    <li key={asset.id} className="cardTry">
-      <Link to={`/Dashboard/${asset.id}`}>
-        <section className="card">
-          <div className="top">
-            <span>
-              <ul className="tags-card">
-                {asset.tags.slice(0, 3).map(tag => (
-                  <li key={v4()}>
-                    {subtags[tag].name}
-                    {` `}
-                  </li>
-                ))}
-              </ul>
-            </span>
-            {renderImage(asset)}
-          </div>
-
-          <div>
-            <div className="bottom">
-              <span>{asset.name}</span>
-              <img src={arrow} className="image2" />
+export default ({ assets, end, subtags }) =>
+  Object.values(assets)
+    .slice(0, end)
+    .map(asset => (
+      <li key={asset.id} className="cardTry">
+        <Link to={`/Dashboard/${asset.id}`}>
+          <section className="card">
+            <div className="top">
+              <span>
+                <ul className="tags-card">
+                  {asset.tags.slice(0, 3).map(tag => (
+                    <li key={v4()} className="tag-name">
+                      {subtags[tag].name}
+                      {` `}
+                    </li>
+                  ))}
+                </ul>
+              </span>
+              {renderImage(asset)}
             </div>
-          </div>
-        </section>
-      </Link>
-    </li>
-  ));
+
+            <div>
+              <div className="bottom">
+                <span className="asset-name">{asset.name}</span>
+                <img src={arrow} className="image2" />
+              </div>
+            </div>
+          </section>
+        </Link>
+      </li>
+    ));
--- client/src/helper/fileImages.js
+++ client/src/helper/fileImages.js
@@ -6,35 +6,40 @@ import zip from "../images/zip.svg";
 import picture from "../images/picture.svg";
 import webm from "../images/video-player.svg";
 import link from "../images/link.svg";
+import back from "../images/left-arrow.svg";
 
-export  default (file)=> {
-   switch (file.filetype) {
+export default file => {
+  switch (file.filetype ? file.filetype : file) {
+  case "video/mp4": {
+    return <img className="img-container" src={mp4} />;
+  }
+  case "application/pdf": {
+    return <img className="img-container" src={pdf} />;
+  }
+  case "video/webm": {
+    return <img className="img-container" src={webm} />;
+  }
+  case "application/zip": {
+    return <img className="img-container" src={zip} />;
+  }
+  case "image/jpeg": {
+    return <img className="img-container" src={picture} />;
+  }
+  case "back-btn": {
+    return (
+      <img className="back-btn" src={back} alt="back button to dashboard" />
+    );
+  }
 
-               case "video/mp4": {
-                   return (<img className="img-container"  src={mp4}  />);
-               }
-      case "application/pdf": {
-                   return (<img className="img-container"  src={pdf}  />);
-               }
-      case "video/webm": {
-                   return (  <img className="img-container" src={webm}  />);
-               }
-      case "application/zip": {
-                   return (  <img className="img-container"  src={zip}  />);
-               }
-      case "image/jpeg": {
-                   return (  <img className="img-container"  src={picture}  />);
-               }
+  case null: {
+    return <img className="img-container" src={link} />;
+  }
 
-      case  null: {
-           return (  <img className="img-container"  src={link}  />);
-               }
-
-      case  "": {
-                   return (  <img className="img-container"  src={link}  />);
-               }
-               default: {
-                   return <div>No asset</div>;
-               }
-           }
-       }
+  case "": {
+    return <img className="img-container" src={link} />;
+  }
+  default: {
+    return <div>No asset</div>;
+  }
+  }
+};
