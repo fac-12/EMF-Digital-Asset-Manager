81f7e50835f2dd82f8517d97881d5bbd7f8adb2b
tundeaji1232 <ajiboye.tunde@gmail.com>
2018-02-15 17:42:31 +0000

    alt tags added relates #38

--- client/src/helper/fileImages.js
+++ client/src/helper/fileImages.js
@@ -11,27 +11,27 @@ export  default (file)=> {
    switch (file.filetype) {
 
                case "video/mp4": {
-                   return (<img className="img-container"  src={mp4}  />);
+                   return (<img className="img-container"  src={mp4}  alt={`${file.filetype}`} />);
                }
       case "application/pdf": {
-                   return (<img className="img-container"  src={pdf}  />);
+                   return (<img className="img-container"  src={pdf}   alt={`${file.filetype}`} />);
                }
       case "video/webm": {
-                   return (  <img className="img-container" src={webm}  />);
+                   return (  <img className="img-container" src={webm}  alt={`${file.filetype}`}  />);
                }
       case "application/zip": {
-                   return (  <img className="img-container"  src={zip}  />);
+                   return (  <img className="img-container"  src={zip}   alt={`${file.filetype}`} />);
                }
       case "image/jpeg": {
-                   return (  <img className="img-container"  src={picture}  />);
+                   return (  <img className="img-container"  src={picture}  alt={`${file.filetype}`} />);
                }
 
       case  null: {
-           return (  <img className="img-container"  src={link}  />);
+           return (  <img className="img-container"  src={link}  alt={`${file.filetype}`} />);
                }
 
       case  "": {
-                   return (  <img className="img-container"  src={link}  />);
+                   return (  <img className="img-container"  src={link}  alt={`${file.filetype}`} />);
                }
                default: {
                    return <div>No asset</div>;
