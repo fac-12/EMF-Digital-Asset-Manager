6c62920a1e0ee708c50da4d8f9210ff7b45ed732
tundeaji1232 <ajiboye.tunde@gmail.com>
2018-02-15 17:25:48 +0000

    keeping the style on the categories

--- client/src/components/Dashboard/FilterOptions.js
+++ client/src/components/Dashboard/FilterOptions.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React , { Component }from "react";
 import { mapValues } from "lodash";
 import { v4 } from "uuid";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
-export default ({ tags, onClick }) => {
-  const tagNames = Object.values(mapValues(tags, o => o.name)).sort();
-
-  tagNames.unshift("ALL");
-
-  return tagNames.map(tag => (
-    <li key={v4()}>
-      <button onClick={onClick} id={tag} className="filter-tag">
-        {tag}
-      </button>
-    </li>
-  ));
+  class FilterOptions extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    selectedCategory:"",
+  };
+}
+onFilter = e => {
+  this.props.setFilter(e ? e.target.id:"ALL");
+  this.setState({selectedCategory: this.props.filters})
 };
+
+ displayCategory= (tags, onClick ) => {
+let tagNames = Object.values(mapValues(tags, o => o.name)).sort();
+
+tagNames.unshift("ALL");
+
+return tagNames.map(tag => (
+
+    <button onClick={onClick} id={tag} className={this.state.selectedCategory===tag ? "filter-selected"  : "filter-tag"} >
+      {tag}
+    </button>
+
+));
+};
+
+
+render(){
+
+  console.log("filter", this.state.selectedCategory);
+  const { tags,onClick }=this.props;
+  // const tags =this.props.tags
+return(
+  <li key={v4()}>
+    {this.displayCategory(tags, this.onFilter())}
+
+  </li>
+)
+
+}
+}
+
+const mapStateToProps = state => ({
+  filters: state.filters
+});
+
+export default connect(null, actions)(FilterOptions);
